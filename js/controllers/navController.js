a2d499f52ccc87ae0eaacc0159a7aa0bd264177d
Matt O <matthew.olson@us.sogeti.com>
2015-07-03 19:16:51 -0400

    lots of restructuring of templates and data

--- js/controllers/navController.js
+++ js/controllers/navController.js
@@ -1,5 +1,9 @@
-comfortCaninesControllers.controller('navController', ['$scope',
-	function($scope) {
-		
+comfortCaninesControllers.controller('navController', ['$scope', '$location',
+	function($scope, $location) {
+		$scope.isActive = function(pathToValidate) {
+			var pathParams = $location.path.split("/");
+			
+			return 	_.some(pathParams, pathToValidate);
+		},
 	}
 ]);
