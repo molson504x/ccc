efd0dff5e9e44e44e198a24ce494b137c1e6c249
Matt O <matthew.olson@us.sogeti.com>
2015-07-03 19:19:43 -0400

    errors

--- js/controllers/navController.js
+++ js/controllers/navController.js
@@ -1,7 +1,7 @@
 comfortCaninesControllers.controller('navController', ['$scope', '$location',
 	function($scope, $location) {
 		$scope.isActive = function(pathToValidate) {
-			var pathParams = $location.path.split("/");
+			var pathParams = $location.path().split("/");
 			
 			return 	_.some(pathParams, pathToValidate);
 		};
