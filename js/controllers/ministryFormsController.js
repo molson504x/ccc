4f89fc6b84484681af6837cb1f639638ad7e15d9
Matt O <matthew.olson@us.sogeti.com>
2015-07-10 09:21:44 -0400

    add dog form updates

--- js/controllers/ministryFormsController.js
+++ js/controllers/ministryFormsController.js
@@ -1,5 +1,40 @@
 comfortCaninesControllers.controller('ministryFormsController', ['$scope',
 	function($scope) {
-		
+		$scope.dogInfo = {
+			name: "",
+			breed: "",
+			gender: "",
+			hasBeenFixed: false,
+			age: null,
+			weight: null,
+			colors: "",
+			vetInfo: {
+				name: "",
+				address: "",
+				phoneNumber: ""
+			},
+			healthIssues: {
+				hasHealthIssues: false,
+				explain: ""
+			},
+			vaccinationsUpToDate: false,
+			preventativeMeds: {
+				flea: false,
+				tick: false,
+				heartWorm: false,
+				other: false,
+				otherExplain: ""
+			},
+			certifications: "",
+			experience: {
+				children: "",
+				publicSettings: "",
+				specialNeeds: ""
+			},
+			otherDogs: {
+				goodWithOtherDogs: false,
+				explain: ""
+			}
+		};
 	}
 ]);
